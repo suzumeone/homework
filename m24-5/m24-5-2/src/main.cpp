02fe190b14ee3266e8c7139efea52303f0550e4e
suzumeone <maillikelol@gmail.com>
2023-07-02 18:18:51 +0600

    m24-5-2 fix

--- m24-5/m24-5-2/src/main.cpp
+++ m24-5/m24-5-2/src/main.cpp
@@ -69,6 +69,7 @@ int main()
                 for (auto name : day.second)
                     std::cout << name << " ";
                 std::cout << std::endl;
+                break;
             }        
         }
     }
