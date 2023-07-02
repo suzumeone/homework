ee0666cb16cd1ce18e83a65894dba312b280273a
suzumeone <maillikelol@gmail.com>
2023-07-02 15:51:19 +0600

    m24-5-3 added

--- m24-5/m24-5-3/src/main.cpp
+++ m24-5/m24-5-3/src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <cstring>
 #include <chrono>
 #include <thread>
 #include <ctime>
@@ -20,19 +22,47 @@ int main()
     std::string time;
     std::cout << "Timer: MM:SS" << std::endl;
     std::tm tm{0};
-    std::cin >> std::get_time(&tm, "%M:%S");
-    while(tm.tm_min > 0 || tm.tm_sec > 0)
+    std::cin >> time;
+    size_t sPos = time.find(":");
+    std::stringstream ss(time);
+    if (sPos != std::string::npos)
     {
-        if(tm.tm_sec == 0)
+        int left = 0, right = 0;
+
+        try
         {
-            tm.tm_min--;
-            tm.tm_sec = 60;
+            left = stoi(time.substr(0, sPos));
+            right = stoi(time.substr(sPos + 1, strlen(time.c_str())));
         }
-        tm.tm_sec--;
-        Clear();
-        std::cout << "Timer: MM:SS" << std::endl;
-        std::cout << std::put_time(&tm, "%M:%S") << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(999));
+        catch (...)
+        {
+            std::cout << "Incorret time format." << std::endl;
+            return 1;
+        }
+
+        if (left >= 0 && left <= 59 && right >= 0 && right <= 59);
+        ss >> std::get_time(&tm, "%M:%S");
+        while (tm.tm_min > 0 || tm.tm_sec > 0)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(999));
+            if (tm.tm_sec == 0)
+            {
+                tm.tm_min--;
+                tm.tm_sec = 60;
+            }
+            tm.tm_sec--;
+            Clear();
+            std::cout << "Timer: MM:SS" << std::endl;
+            std::cout << std::put_time(&tm, "%M:%S") << std::endl;
+
+        }
+        std::cout << "DING! DING! DING!" << std::endl;
     }
-    std::cout << "DING! DING! DING!" << std::endl;
+    else
+    {
+        std::cout << "Incorret time format." << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
